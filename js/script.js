a3f65b32280ebd21d8ebb54392ef6c9bdc29b8a2
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-04 12:46:27 +0100

    JSnack end

--- js/script.js
+++ js/script.js
@@ -2,19 +2,18 @@
  JSnack 1
  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 */
-// const numberOne = parseFloat(prompt("Inserisci il primo numero:"));
-// const numberTwo = parseFloat(prompt("Inserisci il secondo numero:"));
-// let greaterNumber = `I numeri sono uguali.`;
+ const numberOne = parseFloat(prompt("Inserisci il primo numero:"));
+ const numberTwo = parseFloat(prompt("Inserisci il secondo numero:"));
+ let greaterNumber = `I numeri sono uguali.`;
 
-// if (numberOne > numberTwo) {
-//   console.log("Il numero maggiore è: ", numberOne);
-//   greaterNumber = numberOne;
-// } else if (numberTwo > numberOne) {
-//   console.log("Il numero maggiore è: ", numberTwo);
-//   greaterNumber = numberTwo;
-// }
+ if (numberOne > numberTwo) {
+   console.log("Il numero maggiore è: ", numberOne);
+   greaterNumber = numberOne;
+ } else if (numberTwo > numberOne) {
+   console.log("Il numero maggiore è: ", numberTwo);
+   greaterNumber = numberTwo; }
 
-// document.getElementById("greaterNumber").innerHTML += ` ${greaterNumber}`
+ document.getElementById("greaterNumber").innerHTML += ` ${greaterNumber}`
 
 
 // JSnack 3
